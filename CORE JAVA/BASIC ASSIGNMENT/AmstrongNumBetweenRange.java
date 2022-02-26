6a43e8a051b41bc1a304be49c9dd1aefc276ac8b
Vignesh140 <99001880+Vignesh140@users.noreply.github.com>
2022-02-26 20:34:37 +0530

    Add files via upload

---
+++ CORE JAVA/BASIC ASSIGNMENT/AmstrongNumBetweenRange.java
@@ -0,0 +1,19 @@
+package vignesh;
+
+public class AmstrongNumBetweenRange {
+	
+	
+	public static void main(String[] args) {
+		for (int i=100;i<=999;i++) {
+			String a[]=String.valueOf(i).split("");
+			int b=0;
+			for (int j=0;j<a.length;j++) {
+				b += (int)Math.pow(Integer.parseInt(a[j]), 3);
+			}
+			if(i==b)
+				System.out.println(i);
+		}
+
+	}
+
+}
