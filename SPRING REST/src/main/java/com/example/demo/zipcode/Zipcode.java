ed8e6effafc36b99f5ccdf9d17c0192390feee1e
Vignesh140 <99001880+Vignesh140@users.noreply.github.com>
2022-04-02 22:50:33 +0530

    Add files via upload

---
+++ SPRING REST/src/main/java/com/example/demo/zipcode/Zipcode.java
@@ -0,0 +1,26 @@
+package com.example.demo.zipcode;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Zipcode {
+	
+	DetailsRepo repo = new DetailsRepo();
+
+	@RequestMapping(value = "/zipcodes", method = RequestMethod.GET, produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
+	public List<ZipcodeDetails> zipcodes() {
+		return repo.getZipcodes();
+	}
+
+	@RequestMapping(value = "/zipcode/{zipcode}", method = RequestMethod.GET, produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
+	public ZipcodeDetails zipcode(@PathVariable("zipcode") long zip) {
+		return repo.getZipcodeDetails(zip);
+
+	}
+
+}
