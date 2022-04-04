9891450d033cca6da639bd2586673e611065ac9b
Vignesh140 <99001880+Vignesh140@users.noreply.github.com>
2022-04-04 15:44:15 +0530

    Add files via upload

---
+++ SPRINGRESTQ8/src/test/java/com/example/demo/Demomongodb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demomongodb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
