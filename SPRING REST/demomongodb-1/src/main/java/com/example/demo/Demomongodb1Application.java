677054c44640df90eabe7a63da76219d4afa5988
Vignesh140 <99001880+Vignesh140@users.noreply.github.com>
2022-04-04 15:34:55 +0530

    Add files via upload

---
+++ SPRING REST/demomongodb-1/src/main/java/com/example/demo/Demomongodb1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demomongodb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demomongodb1Application.class, args);
+	}
+
+}
